2c192c6e64bade1644169d344aa71fbae84df145
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-09-25 19:54:28 +0530

    Driver Option - Maximize Browser Window

---
+++ concepts/driver_options/01_maximize_browser_window.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def test_open_maximized_browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    try:
+        # Open the Google homepage in the Chrome browser
+        driver.get("https://www.google.com/")
+
+        # Assert that the title of the webpage is equal to "Google"
+        assert driver.title == "Google"
+
+        # Wait for some time (you need to define the waitForSomeTime() function)
+        wait_some_time()
+
+    finally:
+        # Close the Chrome browser
+        driver.quit()
+
+
+# Define your wait_some_dime() function here
+def wait_some_time():
+    # Example: Wait for 5 seconds
+    time.sleep(5)

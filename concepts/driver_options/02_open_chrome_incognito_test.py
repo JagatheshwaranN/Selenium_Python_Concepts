503423596d7d9f29202947f952bea7fb1c17d8d1
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-09-27 10:17:40 +0530

    Driver Option - Chrome Incognito

--- concepts/driver_options/02_open_chrome_incognito_test.py
+++ concepts/driver_options/02_open_chrome_incognito_test.py
@@ -1,34 +1,27 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def test_open_maximized_browser():
+def test_open_chrome_incognito():
     # Create an instance of ChromeOptions to customize Chrome WebDriver's behavior.
     chrome_options = Options()
 
-    # Add an argument to ChromeOptions to start the Chrome browser in maximized mode.
-    chrome_options.add_argument("--start-maximized")
+    # Add an argument to ChromeOptions to start the Chrome browser in incognito mode.
+    chrome_options.add_argument("--incognito")
 
     # Create a new instance of the Chrome WebDriver with the specified options.
     driver = webdriver.Chrome(options=chrome_options)
 
     try:
+        # Maximize the browser window
+        driver.maximize_window()
+
         # Open the Google homepage in the Chrome browser
         driver.get("https://www.google.com/")
 
         # Assert that the title of the webpage is equal to "Google"
         assert driver.title == "Google"
 
-        # Wait for some time
-        wait_some_time()
-
     finally:
         # Close the Chrome browser
         driver.quit()
-
-
-# Define your wait_some_dime() function here
-def wait_some_time():
-    # Example: Wait for 5 seconds
-    time.sleep(5)

d6bebd54de5790780ed3119ed4bd53de3d5a5a29
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-10-03 16:52:13 +0530

    Element Actions - Element Text and Attributes

---
+++ concepts/element_actions/07_element_text_test.py
@@ -0,0 +1,38 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestElementText(unittest.TestCase):
+    # Initialize the driver variable
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        # Initialize the Chrome WebDriver
+        cls.driver = webdriver.Chrome()
+
+        # Maximize the browser window
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Close the driver
+        cls.driver.quit()
+
+    def test_get_element_text(self):
+        # Navigate to the inputs page
+        self.driver.get("https://www.selenium.dev/selenium/web/inputs.html")
+
+        # Find the element using a Tag Name
+        element = self.driver.find_element(By.TAG_NAME, "h1")
+
+        # Get the element text
+        element_text = element.text
+
+        # Assert that the retrieved text match the expected values
+        self.assertEqual(element_text, "Testing Inputs")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ concepts/element_actions/08_element_get_attribute_test.py
@@ -0,0 +1,40 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestElementGetAttribute(unittest.TestCase):
+    # Initialize the driver variable
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        # Initialize the Chrome WebDriver
+        cls.driver = webdriver.Chrome()
+
+        # Maximize the browser window
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Close the driver
+        cls.driver.quit()
+
+    def test_get_element_attribute(self):
+        # Navigate to the inputs page
+        self.driver.get("https://www.selenium.dev/selenium/web/inputs.html")
+
+        # Find the element using a CSS selector
+        element = self.driver.find_element(By.CSS_SELECTOR, "input[name='no_type']")
+
+        # Get the "value" and "name" attributes of the element
+        element_value = element.get_attribute("value")
+        element_name = element.get_attribute("name")
+
+        # Assert that the retrieved attributes match the expected values
+        self.assertEqual(element_value, "input with no type")
+        self.assertEqual(element_name, "no_type")
+
+
+if __name__ == "__main__":
+    unittest.main()

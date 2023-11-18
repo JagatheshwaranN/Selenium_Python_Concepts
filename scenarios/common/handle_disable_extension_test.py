1eaa93df83240ce6f8051bbd181a0e083aa3bd25
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-11-18 12:03:18 +0530

    Handle Disable Extension and TypeAhead Suggestion

---
+++ scenarios/common/handle_disable_extension_test.py
@@ -0,0 +1,41 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class TestHandleDisableExtension(unittest.TestCase):
+    # Initialize the driver variable
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        # Create ChromeOptions object to configure Chrome WebDriver settings
+        chrome_options = Options()
+
+        # Add arguments to ChromeOptions (For disabling extension)
+        chrome_options.add_argument("--disable-extensions")
+
+        # Initialize ChromeDriver with configured options
+        cls.driver = webdriver.Chrome(options=chrome_options)
+
+        # Maximize the browser window
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Close the driver
+        cls.driver.quit()
+
+    def test_handle_disable_extension(self):
+        # Navigate to the Google website
+        self.driver.get("https://www.google.com/")
+
+        # Get the title of the current page
+        title = self.driver.title
+
+        # Verify if the title matches the expected title ('Google')
+        self.assertEqual(title, "Google")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ scenarios/typeahead_suggestion/handle_type_ahead_suggestion_type2_test.py
@@ -0,0 +1,76 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class TestHandleTypeAheadSuggestionType2(unittest.TestCase):
+    # Initialize the driver variable
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        # Initialize the Chrome WebDriver
+        cls.driver = webdriver.Chrome()
+
+        # Maximize the browser window
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Close the driver
+        cls.driver.quit()
+
+    def test_handle_type_ahead_suggestion_type2(self):
+        # Define input text and suggestion to select
+        input_text = 'sca'
+        suggestion_to_select = "Scala"
+
+        # Navigate to the webpage
+        self.driver.get("http://jqueryui.com/autocomplete/")
+
+        # Switch to the frame containing the autocomplete input
+        frame_element = self.driver.find_element(By.CLASS_NAME, "demo-frame")
+        self.driver.switch_to.frame(frame_element)
+
+        # Find the search bar element with ID
+        search_bar = self.driver.find_element(By.ID, "tags")
+
+        # Iterate through each character in 'input_text'
+        for i in range(len(input_text)):
+            # Get the current character
+            current_char = input_text[i]
+
+            # Send the current character to the search bar
+            search_bar.send_keys(str(current_char))
+
+            # Add a delay for UI responsiveness (adjust as needed)
+            time.sleep(0.5)  # Waits for 0.5 seconds between each character input
+
+        # Wait for the type-ahead suggestion list to appear
+        type_ahead_suggestion = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.ID, "ui-id-1"))
+        )
+
+        # Call the method to select a suggestion
+        self.select_suggestion(type_ahead_suggestion, suggestion_to_select)
+
+    def select_suggestion(self, element, text):
+
+        # Loop through each suggestion element in the typeAheadSuggestion list.
+        for suggestion in element.find_elements(By.TAG_NAME, "li"):
+
+            # Check if the text of the suggestion matches the expected SUGGESTION_TO_SELECT.
+            if suggestion.text == text:
+
+                # If a match is found, click on the suggestion and exit the loop.
+                suggestion.click()
+
+                # Break the loop
+                break
+
+
+if __name__ == "__main__":
+    unittest.main()
